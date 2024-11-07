2be3d2934c29cd03daf23286859753398b0691ae
Vladimir Mysko <57433511+vladimirmysko@users.noreply.github.com>
2024-11-07 20:57:23 +0500

    feat: implement internationalization for home page; update content with locale support

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,7 +1,11 @@
+import { getI18n } from '@/locales/server';
+
 import Link from 'next/link';
 import { Logo } from '@/components/logo';
 
-export default function HomePage() {
+export default async function HomePage() {
+  const t = await getI18n();
+
   return (
     <div className="flex flex-col items-center">
       <main className="flex w-full max-w-xl flex-col items-stretch px-5 py-40">
@@ -11,34 +15,23 @@ export default function HomePage() {
             href="#"
             className="rounded text-sm text-gray-1100 underline outline-none focus-visible:ring-2 focus-visible:ring-gray-700 focus-visible:ring-offset-2 focus-visible:ring-offset-gray-100"
           >
-            Login
+            {t('home.login')}
           </Link>
         </nav>
 
         <section className="mt-16 flex flex-col items-stretch gap-1">
-          <h1 className="text-sm/6 font-medium">Bookmarks</h1>
-          <p className="text-sm/6 text-gray-1100">
-            Effortlessly save, organize, and retrieve your essential links. Simple, functional, just
-            enough—no fluff.
-          </p>
+          <h1 className="text-sm/6 font-medium">{t('home.title')}</h1>
+          <p className="text-sm/6 text-gray-1100">{t('home.subtitle')}</p>
         </section>
 
         <section className="mt-6 flex flex-col items-stretch gap-1">
-          <h1 className="text-sm/6 font-medium">About</h1>
-          <p className="text-sm/6 text-gray-1100">
-            Inspired by Rauno Freiberg&apos;s (Basic) Bookmarks, this project is a skill test and
-            portfolio piece. Built to showcase simplicity and functionality. Minimal features, clean
-            design, focused on usability. No tracking, no ads—just an intuitive experience.
-          </p>
+          <h1 className="text-sm/6 font-medium">{t('home.about.title')}</h1>
+          <p className="text-sm/6 text-gray-1100">{t('home.about.paragraph')}</p>
         </section>
 
         <section className="mt-6 flex flex-col items-stretch gap-1">
-          <h1 className="text-sm/6 font-medium">Join</h1>
-          <p className="text-sm/6 text-gray-1100">
-            Free to use, but keep in mind: no guaranteed updates or support. This project is a skill
-            showcase and portfolio piece—built for simplicity, not scale. Dive in if you&apos;re
-            after a clean, no-fuss bookmarking experience.
-          </p>
+          <h1 className="text-sm/6 font-medium">{t('home.join.title')}</h1>
+          <p className="text-sm/6 text-gray-1100">{t('home.join.paragraph')}</p>
         </section>
 
         <section></section>
--- locales/en.ts
+++ locales/en.ts
@@ -3,4 +3,20 @@ export default {
     title: 'Bookmarks',
     description: '',
   },
+  home: {
+    login: 'Login',
+    title: 'Bookmarks',
+    subtitle:
+      'Effortlessly save, organize, and retrieve your essential links. Simple, functional, just enough—no fluff.',
+    about: {
+      title: 'About',
+      paragraph:
+        "Inspired by Rauno Freiberg's (Basic) Bookmarks, this project is a skill test and portfolio piece. Built to showcase simplicity and functionality. Minimal features, clean design, focused on usability. No tracking, no ads—just an intuitive experience.",
+    },
+    join: {
+      title: 'Join',
+      paragraph:
+        "Free to use, but keep in mind: no guaranteed updates or support. This project is a skill showcase and portfolio piece—built for simplicity, not scale. Dive in if you're after a clean, no-fuss bookmarking experience.",
+    },
+  },
 } as const;
